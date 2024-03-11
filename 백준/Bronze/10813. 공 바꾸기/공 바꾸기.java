9799057eb762f5036582967356c5c23c4ef11ed2
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2024-03-11 11:39:22 +0900

    [Bronze II] Title: 공 바꾸기, Time: 248 ms, Memory: 18540 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -6,27 +6,18 @@ public class Main {
         int N = sc.nextInt();
         int M = sc.nextInt();
         int[] input = new int[N];
-        int temp = Integer.MIN_VALUE;
+        int temp = 0;
 
         for (int i = 0; i < N; i++) {
             input[i] = i + 1;
         }
-
         for (int a = 0; a < M; a++) {
-            int i = sc.nextInt() - 1; // 인덱스 조정
-            int j = sc.nextInt() - 1; // 인덱스 조정
-
-            // 인덱스가 배열의 범위를 벗어나는지 확인
-            if (i >= 0 && i < N && j >= 0 && j < N) {
-                temp = input[i];
-                input[i] = input[j];
-                input[j] = temp;
-            } else {
-                System.out.println("Invalid index. Please provide valid indices within the array's bounds.");
-                return; // 프로그램 종료
-            }
+            int i = sc.nextInt() - 1;
+            int j = sc.nextInt() - 1;
+            temp = input[i];
+            input[i] = input[j];
+            input[j] = temp;
         }
-
         for (int k = 0; k < N; k++) {
             System.out.print(input[k] + " ");
         }
