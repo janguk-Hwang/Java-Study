89e85021de136c0666c1c8557c9e8326d5790906
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2024-03-18 11:00:10 +0900

    [Bronze III] Title: 최댓값, Time: 152 ms, Memory: 16040 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -3,35 +3,31 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int[][] matrix = new int[9][9];
+        String[] input = new String[9];
         int maximum = Integer.MIN_VALUE;
+        int i,j = 0;
         int row = 0;
         int col = 0;
-
-        // 행렬 입력 받기
-        for (int i = 0; i < 9; i++) {
-            String[] input = br.readLine().split(" ");
-            for (int j = 0; j < 9; j++) {
+        for(i=0; i<9; i++){
+            input = br.readLine().split(" ");
+            for(j=0; j<9; j++){    
                 matrix[i][j] = Integer.parseInt(input[j]);
-                // 최댓값 갱신
-                if (matrix[i][j] > maximum) {
+                if(matrix[i][j] > maximum){
                     maximum = matrix[i][j];
                     row = i;
                     col = j;
                 }
             }
         }
-
-        // 최댓값 출력
-        bw.write(maximum + "\n");
+        bw.write(String.valueOf(maximum) + "\n");
         bw.write((row + 1) + " " + (col + 1));
-        
-        br.close();
         bw.flush();
         bw.close();
     }
