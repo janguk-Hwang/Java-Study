faff6e4e53553bfe348693c731fda6ea499e78e3
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2024-10-16 21:33:03 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 3124 ms, Memory: 422116 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,48 +1,47 @@
 import java.io.*;
 import java.util.Collections;
-import java.util.PriorityQueue;
 import java.util.HashMap;
+import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
-        int T = Integer.parseInt(br.readLine());  // 테스트 케이스 수
         for (int i = 0; i < T; i++) {
-            int k = Integer.parseInt(br.readLine());  // 연산의 수
-
-            // 최소 힙과 최대 힙
             PriorityQueue<Integer> minHeap = new PriorityQueue<>();
             PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-            HashMap<Integer, Integer> countMap = new HashMap<>();  // 값의 개수를 관리하는 HashMap
+            HashMap<Integer, Integer> count = new HashMap<>();
 
-            for (int j = 0; j < k; j++) {
+            int N = Integer.parseInt(br.readLine());
+            for (int j = 0; j < N; j++) {
                 String[] input = br.readLine().split(" ");
-                String operation = input[0];
-                int num = Integer.parseInt(input[1]);
+                int number = Integer.parseInt(input[1]);
 
-                if (operation.equals("I")) {
-                    // 삽입 연산
-                    minHeap.add(num);
-                    maxHeap.add(num);
-                    countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-                } else if (operation.equals("D")) {
-                    if (num == 1) {
-                        // 최댓값 삭제
-                        deleteTop(maxHeap, countMap);
-                    } else if (num == -1) {
-                        // 최솟값 삭제
-                        deleteTop(minHeap, countMap);
+                if (input[0].equals("I")) {
+                    minHeap.add(number);
+                    maxHeap.add(number);
+                    count.put(number, count.getOrDefault(number, 0) + 1);
+                } else if (input[0].equals("D")) {
+                    if (minHeap.isEmpty() && maxHeap.isEmpty()) {
+                        continue;
+                    }
+                    if (number == 1) {
+                        delete(maxHeap, count);
+                    } else if (number == -1) {
+                        delete(minHeap, count);
                     }
                 }
             }
+            
+            while (!minHeap.isEmpty() && !count.containsKey(minHeap.peek())) {
+                minHeap.poll();
+            }
+            while (!maxHeap.isEmpty() && !count.containsKey(maxHeap.peek())) {
+                maxHeap.poll();
+            }
 
-            // 연산이 끝난 후 힙 정리 (유효하지 않은 값들 제거)
-            cleanup(minHeap, countMap);
-            cleanup(maxHeap, countMap);
-
-            // 결과 출력
-            if (minHeap.isEmpty()) {
+            if (minHeap.isEmpty() || maxHeap.isEmpty()) {
                 System.out.println("EMPTY");
             } else {
                 System.out.println(maxHeap.peek() + " " + minHeap.peek());
@@ -50,25 +49,16 @@ public class Main {
         }
     }
 
-    // 우선순위 큐에서 최댓값이나 최솟값을 삭제하는 함수
-    private static void deleteTop(PriorityQueue<Integer> heap, HashMap<Integer, Integer> countMap) {
+    public static void delete(PriorityQueue<Integer> heap, HashMap<Integer, Integer> count) {
         while (!heap.isEmpty()) {
             int value = heap.poll();
-            if (countMap.containsKey(value) && countMap.get(value) > 0) {
-                // 삭제할 값의 카운트를 줄임
-                countMap.put(value, countMap.get(value) - 1);
-                if (countMap.get(value) == 0) {
-                    countMap.remove(value);
+            if (count.containsKey(value) && count.get(value) > 0) {
+                count.put(value, count.get(value) - 1);
+                if (count.get(value) == 0) {
+                    count.remove(value);
                 }
                 break;
             }
-        }
-    }
-
-    // 힙에서 유효하지 않은 값을 제거하는 함수
-    private static void cleanup(PriorityQueue<Integer> heap, HashMap<Integer, Integer> countMap) {
-        while (!heap.isEmpty() && (!countMap.containsKey(heap.peek()) || countMap.get(heap.peek()) == 0)) {
-            heap.poll();
         }
     }
 }
