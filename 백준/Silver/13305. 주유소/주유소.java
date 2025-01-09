9061a71f014738a76e53dff32d1bc977a6f99299
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-01-09 20:14:11 +0900

    [Silver III] Title: 주유소, Time: 400 ms, Memory: 36100 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,36 +1,33 @@
 import java.io.*;
 import java.util.*;
-import java.lang.Math;
 
 public class Main {
-    static int[] dist;
-    static int[] cost;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        dist = new int[N-1];
-        cost = new int[N];
+        long[] dist = new long[N-1];
+        long[] cost = new long[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         for(int i = 0; i < N - 1; i++) {
-            dist[i] = Integer.parseInt(st.nextToken());
+            dist[i] = Long.parseLong(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine(), " ");
 
         for(int i = 0; i < N; i++) {
-            cost[i] = Integer.parseInt(st.nextToken());
+            cost[i] = Long.parseLong(st.nextToken());
         }
 
-        int sum = 0;
-        int minCost = Integer.MAX_VALUE;
+        long sum = 0;
+        long minCost = cost[0];
 
         for(int i = 0; i < N - 1; i++) {
             if(cost[i] < minCost) {
                 minCost = cost[i];
             }
-            sum += (minCost*dist[i]);
+            sum += (minCost * dist[i]);
         }
 
         System.out.println(sum);
