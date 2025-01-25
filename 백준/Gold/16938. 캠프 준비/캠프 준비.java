7ae505f419b9d580c1aa1ff02b5bd66417d20ec3
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-01-25 15:32:21 +0900

    [Gold V] Title: 캠프 준비, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.java
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.java
@@ -20,29 +20,34 @@ public class Main {
     String[] input = br.readLine().split(" ");
     Ai = new int[N];
     for(int i = 0; i < N; i++) {
-        Ai[i] = Integer.parseInt(input[i]);
+        Ai[i] = Integer.parseInt(input[i]); //난이도 배열
     }
 
+    //난이도를 정렬하면 그냥 순회하면서 조건에 맞는 지 확인하면서 변수를 증가시키면 된다.
     Arrays.sort(Ai);
 
     func(0, 0, 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+
     System.out.println(result);
-    
     }
 
-    
-    
-    public static void func(int idx, int cnt, int sum, int max, int min) {
+
+    //조건이 맞는 지 확인하는 함수
+    //함수를 한 번 실행하면 
+    public static void func(int index, int count, int sum, int max, int min) {
         //문제가 2개 이상이고, L,R,X 조건이 다 맞으면 result 증가
-        if (cnt >= 2) {
+        if (count >= 2) {
             if (sum >= L && sum <= R && max - min >= X) {
                 result += 1;
             }
         }
 
         //idx, cnt는 증가시키고 sum, max, min은 갱신하면서 재귀함수 호출
-        for (int i = idx; i < N; i++) {
-            func(i + 1, cnt + 1, sum + Ai[i], Math.max(max, Ai[i]), Math.min(min, Ai[i]));
+        //index를 증가시켜서 다음 문제부터 선택하도록 호출
+        //재귀와 반복문을 통해서 모든 경우의 수를 탐색할 수 있음
+        for (int i = index; i < N; i++) {
+            //func(1,0,~~~), func(2,0,~~~), func(3,0,~~~)
+            func(i + 1, count + 1, sum + Ai[i], Math.max(max, Ai[i]), Math.min(min, Ai[i]));
         }
     }
 }
