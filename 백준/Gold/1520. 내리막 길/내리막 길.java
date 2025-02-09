6f241e70674fc3dd2e10a8e4d295c1930d921a75
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-02-09 23:50:49 +0900

    [Gold III] Title: 내리막 길, Time: 348 ms, Memory: 35564 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -5,9 +5,10 @@ public class Main {
     static int m, n;
     static int[][] matrix;
     static int[][] dp;
-    static int[] dr = {-1, 0, 1, 0}; // 상, 우, 하, 좌
-    static int[] dc = {0, 1, 0, -1}; 
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
 
+    // DFS 방식으로 경로를 찾고 동적 계획법을 통해 이미 계산된 결과를 재사용
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -17,7 +18,7 @@ public class Main {
 
         matrix = new int[m][n];
         dp = new int[m][n];
-
+        
         // dp 배열을 -1로 초기화 (방문 여부 확인용)
         for (int i = 0; i < m; i++) {
             Arrays.fill(dp[i], -1);
@@ -40,19 +41,20 @@ public class Main {
             return 1;
         }
 
-        // 이미 방문한 적 있으면 해당 값을 반환
+        // 이미 계산된 경로가 있으면 그 값을 반환
+        // 일반 dp 문제처럼 메모라이제이션
         if (dp[r][c] != -1) {
             return dp[r][c];
         }
 
         dp[r][c] = 0; // 초기화 후 탐색 시작
 
-        // 4방향 탐색 (상, 우, 하, 좌)
+        // 4방향으로 탐색
         for (int i = 0; i < 4; i++) {
             int nr = r + dr[i];
             int nc = c + dc[i];
 
-            // 범위 내에 있고 내리막길인 경우만 이동
+            // 배열 범위 내에서, 현재 위치보다 높은 곳으로만 이동
             if (nr >= 0 && nr < m && nc >= 0 && nc < n && matrix[r][c] > matrix[nr][nc]) {
                 dp[r][c] += dfs(nr, nc);
             }
