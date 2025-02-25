fb6ed40f076320325e2974c4c67d6d941c9a7c78
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-02-25 18:12:42 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,25 +1,21 @@
-class Solution{
-    //자연수를 2진수로 변화한 값에서의 1의 개수를 반환하는 함수
-    public int countfunc(int num){
-            int count = 0;
-            while(num > 0){
-                if(num % 2 == 1){ //끝 비트가 1이면
-                    count++;
-                }
-                num /= 2; //오른쪽으로 한 비트 이동
+public int solution(int n) {
+    int countOnes(int num) {
+        int count = 0;
+        while (num > 0) {
+            if (num % 2 == 1) { // 끝 비트가 1이면
+                count++;
             }
-            return count;
-    }
-    
-    public int solution(int n){
-        int nextnum = n+1;
-
-        //다음 숫자와 1의 개수가 같지않으면 다음 숫자로 넘어감
-        while(countfunc(nextnum) != countfunc(n)){
-            nextnum++;
+            num /= 2; // 오른쪽으로 한 비트 이동 (나누기 2)
         }
-
-        //1의 개수가 같으면 while문을 나와서 실행되어 nextnum을 반환
-        return nextnum;
+        return count;
     }
+
+    int targetCount = countOnes(n);
+    int nextNum = n + 1;
+
+    while (countOnes(nextNum) != targetCount) {
+        nextNum++;
+    }
+
+    return nextNum;
 }
