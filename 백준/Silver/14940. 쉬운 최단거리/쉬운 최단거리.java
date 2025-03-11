8c1ae4224bc2579e58223577913480f39db24478
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-03-12 00:37:27 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 580 ms, Memory: 48772 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -11,67 +11,56 @@ public class Main {
     static int[][] dist;
     static int[] dr = {1, 0, -1, 0};    // 상하좌우의 순서가 바뀌어도 답이 같은가? 그렇다면 이유는?
     static int[] dc = {0, 1, 0, -1};
+    static int goalRow, goalCol;
+    static ArrayDeque<int[]> q = new ArrayDeque<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());   // 세로의 크기
         m = Integer.parseInt(st.nextToken());   // 가로의 크기
         matrix = new int[n][m];
         dist = new int[n][m];
+        visited = new boolean[n][m];
 
         //입력 받기 (0: 갈 수 없는 땅, 1: 갈 수 있는 땅, 2: 도착점)
         for (int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine(), " ");
             for (int j=0; j<m; j++){
                 matrix[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        // 도착점 찾기
-        int goalRow = -1, goalCol = -1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 2) {
-                    goalRow = i;
-                    goalCol = j;
-                    break;
+                if(matrix[i][j] == 2){
+                    q.add(new int[]{i, j});
+                    visited[i][j] = true;
                 }
             }
-            if (goalRow != -1) break; // 도착점 찾으면 종료
         }
 
         // BFS 시작
-        bfs(goalRow, goalCol);
+        bfs();
 
         // 결과 출력
         for(int i=0; i<n; i++) {
             for (int j = 0; j < m; j++) {
                 // 갈 수 있는 땅이지만 dist가 0이면 갈 수 없는 땅
                 if (matrix[i][j] == 1 && dist[i][j] == 0) {
-                    System.out.print(-1 + " ");
-                } 
+                    sb.append(-1).append(" ");
+                }
                 else {
-                    System.out.print(dist[i][j] + " ");
+                    sb.append(dist[i][j]).append(" ");
                 }
             }
-            System.out.println();
+            sb.append("\n");
         }
+
+        System.out.print(sb);
     }
 
     // BFS로 도착점에서 각 위치까지의 거리 계산
-    public static void bfs(int r, int c){
-        visited = new boolean[n][m];
-        Queue<int[]> q = new LinkedList<int[]>();  // 큐 생성
-        q.add(new int[]{r, c});  // 도착점부터 시작
-
-        // 도착점의 거리는 0으로 초기화
-        dist[r][c] = 0;
-        visited[r][c] = true;
-
+    public static void bfs(){
         while(!q.isEmpty()){
             int[] cur = q.poll();   // r, c가 계속 값이 이동해야 함으로 q에서 뽑아서 현재 좌표를 할당
-            r = cur[0];
-            c = cur[1];
+            int r = cur[0];
+            int c = cur[1];
 
             // 상하좌우 탐색
             for (int i=0; i<4; i++){
