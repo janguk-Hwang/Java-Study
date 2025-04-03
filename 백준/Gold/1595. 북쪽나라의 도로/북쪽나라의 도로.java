c7ba44d74d347339411fac60b87d0a10eafdf540
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-04-04 00:24:36 +0900

    [Gold IV] Title: 북쪽나라의 도로, Time: 152 ms, Memory: 17128 KB -BaekjoonHub

--- 백준/Gold/1595. 북쪽나라의 도로/북쪽나라의 도로.java
+++ 백준/Gold/1595. 북쪽나라의 도로/북쪽나라의 도로.java
@@ -3,49 +3,48 @@ import java.io.*;
 
 public class Main {
     static class Edge {
-        int to, cost;
+        int dest, cost;
 
-        public Edge(int to, int cost) {
-            this.to = to;
+        public Edge(int dest, int cost) {
+            this.dest = dest;
             this.cost = cost;
         }
     }
 
-    static int n, maxDist, farthestNode;
-    static ArrayList<Edge>[] adjList;
+    static int maxDist, farthestNode;
+    static ArrayList<Edge>[] adjList;   // 연결되어 있는 도시를 담는 리스트
     static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line;
         adjList = new ArrayList[10001];
         for (int i = 0; i < 10001; i++) {
             adjList[i] = new ArrayList<>();
         }
 
-        int maxNode = 0;
+        String line;
         while ((line = br.readLine()) != null && !line.isEmpty()) {
             StringTokenizer st = new StringTokenizer(line);
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
 
-            adjList[u].add(new Edge(v, cost));
-            adjList[v].add(new Edge(u, cost));
-            maxNode = Math.max(maxNode, Math.max(u, v));
+            // 양방향 추가
+            adjList[s].add(new Edge(e, cost));
+            adjList[e].add(new Edge(s, cost));
         }
 
         visited = new boolean[10001];
         maxDist = 0;
         dfs(1, 0);
-
-        visited = new boolean[10001];
+        
+        visited = new boolean[10001];   // visited, maxDist 초기화하고 dfs 한번 더 실행
         maxDist = 0;
         dfs(farthestNode, 0);
 
         System.out.println(maxDist);
     }
 
-    static void dfs(int node, int dist) {
+    static void dfs(int node, int dist) {   // 현재 방문 중인 노드의 번호, 시작 노드부터 현재 노드까지의 거리
         visited[node] = true;
         if (dist > maxDist) {
             maxDist = dist;
@@ -53,8 +52,8 @@ public class Main {
         }
 
         for (Edge edge : adjList[node]) {
-            if (!visited[edge.to]) {
-                dfs(edge.to, dist + edge.cost);
+            if (!visited[edge.dest]) {
+                dfs(edge.dest, dist + edge.cost);
             }
         }
     }
