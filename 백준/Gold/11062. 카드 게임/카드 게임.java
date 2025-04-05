966ac6f80cd36ed7197581a70863da660152c839
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-04-06 02:42:39 +0900

    [Gold III] Title: 카드 게임, Time: 332 ms, Memory: 86384 KB -BaekjoonHub

--- 백준/Gold/11062. 카드 게임/카드 게임.java
+++ 백준/Gold/11062. 카드 게임/카드 게임.java
@@ -3,7 +3,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int[] card;
-    static int[][] dp;
+    static int[][] dp;  // 해당 구간에서 근우가 얻을 수 있는 최댓값을 저장
     static int n, t;
     static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,24 +18,29 @@ public class Main {
                 card[j] = Integer.parseInt(st.nextToken());
             }
             solve(0, n-1, true);
-            System.out.println(dp[0][n-1]);
+            System.out.println(dp[0][n-1]); // n개의 카드에서 근우가 얻을 수 있는 최대 점수 출력
         }
     }
 
+    // left ~ right까지 카드가 있을 때 근우가 얻을 수 있는 최대 점수를 반환
+    // solve 함수가 실행된다는 것은 각자 최선의 선택으로 카드를 뽑는다는 의미
     static int solve(int left, int right, boolean turn) {
-
-        if(left> right) return 0;
+        // 종료 조건
+        if(left > right) return 0;
+        // 메모이제이션
         if(dp[left][right] != 0) return dp[left][right];
 
         // 근우
+        // 근우가 얻을 수 있는 최대 점수는 왼쪽카드를 선택했을 때 명우가 나머지에서 최선의 선택을 하는 경우와
+        // 오른쪽카드를 선택했을 때 명우가 나머지에서 최선의 선택을 하는 경우 중에 더 큰 값이다.
         if(turn) {
-            return dp[left][right] = Math.max(card[left] + solve(left+1, right, false),
-                    card[right] + solve(left, right-1, false));
+            return dp[left][right] = Math.max(card[left] + solve(left+1, right, false), card[right] + solve(left, right-1, false));
         }
         // 명우
+        // dp는 근우의 점수를 의미하기 때문에 근우처럼 card[]를 더하지 않는다.
+        // dp값은 명우가 왼쪽카드를 선택했을 경우와 오른쪽카드를 선택했을 경우 중 더 작은 값이다. 명우는 dp값(근우의 점수)을 낮춰야 하기 때문
         else {
             return dp[left][right] = Math.min(solve(left+1, right, true), solve(left, right-1, true));
         }
-
     }
 }
