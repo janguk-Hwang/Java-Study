7bf8058e8c8fdcbf2a5d48cd45ddf8f5254aee00
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-04-10 18:28:02 +0900

    [Gold V] Title: HTML, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/6581. HTML/HTML.java
+++ 백준/Gold/6581. HTML/HTML.java
@@ -1,44 +1,52 @@
+import java.util.*;
 import java.io.*;
 
+// 마지막 줄은 개행 문자로 끝난다.
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder output = new StringBuilder();
+        sb = new StringBuilder();
         StringBuilder line = new StringBuilder();
+        String str;
 
-        String inputLine;
-        while ((inputLine = br.readLine()) != null) {
-            // 입력 줄에서 \t, 여러 공백 포함된 것들 하나의 공백으로
-            inputLine = inputLine.replaceAll("[\\s]+", " ");
-            // 태그와 단어를 구분하기 위해 split
-            String[] tokens = inputLine.split(" ");
+        while ((str = br.readLine()) != null) {
+            // 여러 공백, 탭, 개행 문자 등을 하나의 공백으로 처리
+            str = str.replaceAll("[\\s]+", " ");
+            st = new StringTokenizer(str, " ", false);
 
-            for (String token : tokens) {
-                if (token.equals("")) continue; // 공백 무시
+            while (st.hasMoreTokens()) {
+                String s = st.nextToken();
 
-                if (token.equals("<br>")) {
-                    output.append(line.toString().trim()).append("\n");
+                if (s.equals("")) continue;
+
+                if (s.equals("<br>")) {
+                    sb.append(line.toString().trim()).append("\n");
                     line.setLength(0);
-                } else if (token.equals("<hr>")) {
+                } else if (s.equals("<hr>")) {
                     if (line.length() > 0) {
-                        output.append(line.toString().trim()).append("\n");
+                        sb.append(line.toString().trim()).append("\n");
                         line.setLength(0);
                     }
-                    output.append("-".repeat(80)).append("\n");
+                    sb.append("-".repeat(80)).append("\n");
                 } else {
-                    if (line.length() + token.length() + 1 > 80) {
-                        output.append(line.toString().trim()).append("\n");
+                    if (line.length() + s.length() + 1 > 80) {
+                        sb.append(line.toString().trim()).append("\n");
                         line.setLength(0);
                     }
                     if (line.length() > 0) line.append(" ");
-                    line.append(token);
+                    line.append(s);
                 }
             }
         }
+
+        // 마지막 줄 처리
         if (line.length() > 0) {
-            output.append(line.toString().trim()).append("\n");
+            sb.append(line.toString().trim()).append("\n");
         }
 
-        System.out.print(output.toString());
+        System.out.print(sb.toString());
     }
 }
