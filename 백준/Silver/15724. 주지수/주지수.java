abb92ac86f00ec1c4680cf74178ac633ca2c7ab2
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-05-04 23:01:53 +0900

    [Silver I] Title: 주지수, Time: 644 ms, Memory: 115448 KB -BaekjoonHub

--- 백준/Silver/15724. 주지수/주지수.java
+++ 백준/Silver/15724. 주지수/주지수.java
@@ -18,17 +18,9 @@ public class Main {
             st = new StringTokenizer(br.readLine(), " ");
             for(int j=1; j<m+1; j++){
                 matrix[i][j] = Integer.parseInt(st.nextToken());    // 1-based
-            }
-        }
-
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<m+1; j++){
-                // 누적합은 누적합을 이용해 구한다.
-                // 한행 위의 값 + 한열 앞의 값 + matrix[i][j] - prefixSum[i-1][j-1]
                 prefixSum[i][j] = prefixSum[i-1][j] + prefixSum[i][j-1] + matrix[i][j] - prefixSum[i-1][j-1];
             }
         }
-
         k = Integer.parseInt(br.readLine());
         for(int i=0; i<k; i++){
             st = new StringTokenizer(br.readLine(), " ");
@@ -36,7 +28,6 @@ public class Main {
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-
             // y가 열, x가 행
             sb.append(prefixSum[x2][y2] - prefixSum[x2][y1-1] - prefixSum[x1-1][y2] + prefixSum[x1-1][y1-1]).append("\n");
         }
