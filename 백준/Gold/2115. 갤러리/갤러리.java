7a35b942e7ffb69b7fcb0991621a7edd736912b3
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-06-05 17:23:54 +0900

    [Gold V] Title: 갤러리, Time: 272 ms, Memory: 23332 KB -BaekjoonHub

--- 백준/Gold/2115. 갤러리/갤러리.java
+++ 백준/Gold/2115. 갤러리/갤러리.java
@@ -13,50 +13,44 @@ public class Main {
         matrix = new char[n][m];
         for(int i=0; i<n; i++) matrix[i] = br.readLine().toCharArray();
         int temp1 = 0; int temp2 = 0; int total = 0;
-        for (int i = 0; i < n - 1; i++) {
-            temp1 = 0; temp2 = 0;
+        for(int i=0; i<n-1; i++){
             for(int j=0; j<m; j++){
-                if(matrix[i][j] == 'X' && matrix[i + 1][j] == '.'){
+                if(matrix[i][j] == 'X' && matrix[i+1][j] == '.'){
                     temp1++;
-                    continue;
                 }
-                total += temp1 / 2;
-                temp1 = 0;
-            }
-            total += temp1 / 2;
-            for(int j=0; j<m; j++){
-                if(matrix[i][j] == '.' && matrix[i + 1][j] == 'X'){
+                else{
+                    total += temp1 / 2;
+                    temp1 = 0;
+                }
+                if(matrix[i][j] == '.' && matrix[i+1][j] == 'X'){
                     temp2++;
-                    continue;
                 }
-                total += temp2 / 2;
-                temp2 = 0;
+                else{
+                    total += temp2 / 2;
+                    temp2 = 0;
+                }
             }
-            total += temp2 / 2;
         }
 
         temp1 = 0; temp2 = 0;
-        for (int i = 0; i < m - 1; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[j][i] == 'X' && matrix[j][i + 1] == '.') {
+        for(int i=0; i<m-1; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[j][i] == 'X' && matrix[j][i+1] == '.'){
                     temp1++;
-                    continue;
                 }
-                total += temp1 / 2;
-                temp1 = 0;
-            }
-            total += temp1 / 2;
-            for (int j = 0; j < n; j++) {
-                if (matrix[j][i] == '.' && matrix[j][i + 1] == 'X') {
+                else{
+                    total += temp1 / 2;
+                    temp1 = 0;
+                }
+                if(matrix[j][i] == '.' && matrix[j][i+1] == 'X'){
                     temp2++;
-                    continue;
                 }
-                total += temp2 / 2;
-                temp2 = 0;
+                else{
+                    total += temp2 /2;
+                    temp2 = 0;
+                }
             }
-            total += temp2 / 2;
         }
-
         System.out.print(total);
     }
 }
