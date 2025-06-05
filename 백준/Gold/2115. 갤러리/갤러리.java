68ee5d0e3ef9df805964c275ac64fb29797aae52
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-06-05 17:22:27 +0900

    [Gold V] Title: 갤러리, Time: 292 ms, Memory: 22964 KB -BaekjoonHub

---
+++ 백준/Gold/2115. 갤러리/갤러리.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static char[][] matrix;
+    static int n, m;
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        matrix = new char[n][m];
+        for(int i=0; i<n; i++) matrix[i] = br.readLine().toCharArray();
+        int temp1 = 0; int temp2 = 0; int total = 0;
+        for (int i = 0; i < n - 1; i++) {
+            temp1 = 0; temp2 = 0;
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 'X' && matrix[i + 1][j] == '.'){
+                    temp1++;
+                    continue;
+                }
+                total += temp1 / 2;
+                temp1 = 0;
+            }
+            total += temp1 / 2;
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == '.' && matrix[i + 1][j] == 'X'){
+                    temp2++;
+                    continue;
+                }
+                total += temp2 / 2;
+                temp2 = 0;
+            }
+            total += temp2 / 2;
+        }
+
+        temp1 = 0; temp2 = 0;
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[j][i] == 'X' && matrix[j][i + 1] == '.') {
+                    temp1++;
+                    continue;
+                }
+                total += temp1 / 2;
+                temp1 = 0;
+            }
+            total += temp1 / 2;
+            for (int j = 0; j < n; j++) {
+                if (matrix[j][i] == '.' && matrix[j][i + 1] == 'X') {
+                    temp2++;
+                    continue;
+                }
+                total += temp2 / 2;
+                temp2 = 0;
+            }
+            total += temp2 / 2;
+        }
+
+        System.out.print(total);
+    }
+}
