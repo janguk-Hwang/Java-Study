1eff454e45324285793c8c20f0098837005c09b9
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-06-08 16:52:55 +0900

    [Gold I] Title: 서로소 조합, Time: 388 ms, Memory: 48328 KB -BaekjoonHub

--- 백준/Gold/33916. 서로소 조합/서로소 조합.java
+++ 백준/Gold/33916. 서로소 조합/서로소 조합.java
@@ -2,78 +2,71 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int t;
     static int MAX = 5000;
     static List<Integer> primes = new ArrayList<>();
     static boolean[] isNotPrime = new boolean[MAX + 1];
-
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
-        sieve();
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-
-        int primeCount = primes.size();
-        int[] a = new int[primeCount];
-        int[] b = new int[primeCount];
-
-        while (T-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        prime();
+        t = Integer.parseInt(br.readLine());
+        int primeSize = primes.size();
+        while(t-- > 0){
+            st = new StringTokenizer(br.readLine());
             int n1 = Integer.parseInt(st.nextToken());
             int r1 = Integer.parseInt(st.nextToken());
             int n2 = Integer.parseInt(st.nextToken());
             int r2 = Integer.parseInt(st.nextToken());
+            int[] a = new int[primeSize];
+            int[] b = new int[primeSize];
+            calcPrimeCnt(n1, r1, a);
+            calcPrimeCnt(n2, r2, b);
 
-            Arrays.fill(a, 0);
-            Arrays.fill(b, 0);
-
-            getPrimeExponentDiff(n1, r1, a);
-            getPrimeExponentDiff(n2, r2, b);
-
-            boolean coprime = true;
-            for (int i = 0; i < primeCount; i++) {
-                if (a[i] > 0 && b[i] > 0) {
-                    coprime = false;
+            boolean flag = true;
+            for(int i=0; i<primeSize; i++){
+                if(a[i] > 0 && b[i] > 0){   // 두 조합에 대해 공통된 소수가 있는 경우
+                    flag = false;
                     break;
                 }
             }
-
-            sb.append(coprime ? "1\n" : "0\n");
+            sb.append(flag ? 1 : 0).append("\n");
         }
-
         System.out.print(sb);
     }
 
-    static void sieve() {
+    public static void prime() {
         isNotPrime[0] = isNotPrime[1] = true;
-        for (int i = 2; i <= MAX; i++) {
-            if (!isNotPrime[i]) {
+        for(int i=2; i<=MAX; i++){
+            if(!isNotPrime[i]){
                 primes.add(i);
-                for (int j = i * 2; j <= MAX; j += i) {
+                for(int j=i*2; j<=MAX; j+=i){   // i의 배수들은 소수가 아님
                     isNotPrime[j] = true;
                 }
             }
         }
     }
 
-    static void getPrimeExponentDiff(int n, int r, int[] arr) {
-        addFactorialExponents(arr, n, 1);
-        addFactorialExponents(arr, r, -1);
-        addFactorialExponents(arr, n - r, -1);
+    // r!, (n-r)!, n!
+    public static void calcPrimeCnt(int n, int r, int[] arr) {
+        legendre(arr, n, 1);
+        legendre(arr, r, -1);
+        legendre(arr, n - r, -1);
     }
 
-    static void addFactorialExponents(int[] arr, int n, int cnt) {
-        for (int i = 0; i < primes.size(); i++) {
+    public static void legendre(int[] arr, int n, int cnt) {
+        for(int i=0; i<primes.size(); i++){
             int p = primes.get(i);
-            if (p > n) break;
-
+            if(p > n) break;    // n보다 큰 소수는 n!에서 나올 수 없음
             int exp = 0;
-            int k = n;
-            while (k > 0) {
-                exp += k / p;
-                k /= p;
+            int temp = n;
+            // 르장드르의 공식
+            while(temp > 0){
+                exp += temp / p;
+                temp /= p;
             }
-            arr[i] += exp * cnt;
+            arr[i] += exp * cnt;    // arr[2]에는 소수 2가 n!에 나오는 횟수가 저장
         }
     }
 }
