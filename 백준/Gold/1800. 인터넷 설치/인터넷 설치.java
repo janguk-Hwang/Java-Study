00e2f36c8bf7f84268f1a3b05e42f78506436543
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-06-14 03:51:32 +0900

    [Gold I] Title: 인터넷 설치, Time: 268 ms, Memory: 22380 KB -BaekjoonHub

--- 백준/Gold/1800. 인터넷 설치/인터넷 설치.java
+++ 백준/Gold/1800. 인터넷 설치/인터넷 설치.java
@@ -24,16 +24,24 @@ public class Main {
             max = Math.max(max, c);
         }
 
+        // 결정 문제: 상한선을 mid로 설정했을 때, 1번 컴퓨터에서 n번 컴퓨터까지
+        // k개 이하의 비싼 간선(mid보다 비싼 간선)으로 연결이 가능한가?
+        // mid보다 비싼 간선이 k개보다 많으면 불가능
+        // mid보다 비싼 간선이 k개로 커버가능하면 가능
+        // fffffffffffffffttttttttttttttttttttttt
         int answer = -1;
         int start = -1; int end = max;
         while(start + 1 < end){
             int mid = (start + end) / 2;
-            if(dijkstra(mid)){
+            if(dijkstra(mid)){      // true가 반환되면 더 작은 mid값으로 탐색해본다.
                 end = mid;
                 answer = mid;
             }
             else start = mid;
         }
+        // 1번과 n번을 잇는 것이 가능하면 다익스트라 함수의 반환값이 true인 mid값이 항상 존재한다.
+        // mid == max이면 dist[n]은 0이 된다. k는 최소 0이므로 true를 반환한다.
+        // 그러므로 1번과 n번을 이을 수 없으면 answer은 초기값 그대로 -1을 유지한다.
         System.out.print(answer);
     }
 
@@ -57,6 +65,8 @@ public class Main {
                 }
             }
         }
+        // 사용된 mid보다 긴 간선의 수가 k보다 작거나 같은지 여부 반환 -> true이면 mid길이로 연결이 가능
+        // 가능하므로 mid를 더 낮춰서 탐색해본다.
         return dist[n] <= k;
     }
 
