dd096e3b1127c6167ad5b395b191b2437455415b
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-07-07 00:12:14 +0900

    [Gold IV] Title: 부분합, Time: 240 ms, Memory: 23560 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -3,39 +3,25 @@ import java.util.*;
 
 public class Main {
     static int n, s, minLength;
-    static long[] arr, prefixSum;
+    static int[] arr;
     static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         s = Integer.parseInt(st.nextToken());
-        arr = new long[n]; prefixSum = new long[n + 1];
+        arr = new int[n];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            prefixSum[i + 1] = prefixSum[i] + arr[i];
-        }
+        for(int i=0; i<n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        long sum = 0; int start = 0;
         minLength = 100_001;
-        // 결정 문제: 합이 s보다 큰가?
-        // ffffffftttttttt
-        for(int left=0; left<n; left++){
-            int start = left;
-            int end = n;
-            while(start + 1 < end){
-                int mid = (start + end) / 2;
-                long sum = prefixSum[mid] - prefixSum[left];
-                if(sum >= s) end = mid;
-                else start = mid;
-            }
-            if(end <= n && prefixSum[end] - prefixSum[left] >= s){
-                minLength = Math.min(minLength, end - left);
+        for(int end=0; end<n; end++){
+            sum += arr[end];
+            while(sum >= s){
+                minLength = Math.min(minLength, end - start + 1);
+                sum -= arr[start++];
             }
         }
-        if(minLength == 100_001){
-            System.out.print(0);
-            return;
-        }
-        System.out.print(minLength);
+        System.out.println(minLength == 100_001 ? 0 : minLength);
     }
 }
