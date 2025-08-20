1958f26a8303cc266db1c10ad4ff5a439b8b7239
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-08-20 16:26:46 +0900

    [Gold IV] Title: 도서관, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.java
+++ 백준/Gold/1461. 도서관/도서관.java
@@ -11,9 +11,11 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        int max = 0;
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
             int t = Integer.parseInt(st.nextToken());
+            max = Math.max(max, Math.abs(t));
             if(t < 0){
                 left.add(-t);
                 continue;
@@ -23,16 +25,13 @@ public class Main {
         Collections.sort(left, Collections.reverseOrder());
         Collections.sort(right, Collections.reverseOrder());
         rst = 0;
-        int max = 0;
         // m개씩 들고 왕복(음수)
         for(int i=0; i<left.size(); i+=m){
             rst += left.get(i) * 2;
-            max = Math.max(max, left.get(i));
         }
         // (양수)
         for(int i=0; i<right.size(); i+=m){
             rst += right.get(i) * 2;
-            max = Math.max(max, right.get(i));
         }
         // 마지막은 돌아오지 않아도 됨
         rst -= max;
