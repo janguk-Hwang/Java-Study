1ef811cb3c2154cc8667466dd5a9055ce49e6aac
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-10-16 17:21:41 +0900

    [Bronze IV] Title: $A + B - C$, Time: 104 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.java
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.java
@@ -3,13 +3,16 @@ import java.util.*;
 
 public class Main {
     static int a, b, c;
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
         a = Integer.parseInt(br.readLine());
         b = Integer.parseInt(br.readLine());
         c = Integer.parseInt(br.readLine());
-        System.out.println((a + b - c));
+        sb.append((a + b - c)).append("\n");
         String s = String.valueOf(a).concat(String.valueOf(b));
-        System.out.print(Integer.parseInt(s) - c);
+        sb.append(Integer.parseInt(s) - c);
+        System.out.print(sb);
     }
 }
