ef89f99c0d297c047b197e3865a10d802a9b299f
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-12-20 21:11:38 +0900

    [Gold III] Title: 줄 세우기, Time: 400 ms, Memory: 49412 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,57 +1,38 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
-public class Main{
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String[] in = br.readLine().split(" ");
-        int N = Integer.parseInt(in[0]);
-        int M = Integer.parseInt(in[1]);
-
-        //진입차수
-        int[] edgeCount =new int[N + 1];
-        //위상정렬에 사용할 그래프 2차원 리스트
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-        for (int i = 0; i <= N+1; i++) {
-            graph.add(new ArrayList<Integer>());
+public class Main {
+    static Queue<Integer> q = new LinkedList<>();
+    static int n, m;
+    static ArrayList<Integer>[] adj;
+    static int[] indegree;
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        adj = new ArrayList[n + 1];
+        for(int i=1; i<=n; i++) adj[i] = new ArrayList<>();
+        indegree = new int[n + 1];
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            adj[a].add(b);
+            indegree[b]++;
         }
-        
-        for (int i = 0; i < M; i++) {
-            String[] temp = br.readLine().split(" ");
-            graph.get(Integer.parseInt(temp[0])).add(Integer.parseInt(temp[1]));
-            edgeCount[Integer.parseInt(temp[1])]++;
-        }
-
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 1; i < edgeCount.length; i++) {
-            if (edgeCount[i] == 0) {
-                q.offer(i);
+        for(int i=1; i<=n; i++) if(indegree[i] == 0) q.offer(i);
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            sb.append(cur).append(" ");
+            for(int i : adj[cur]){
+                indegree[i]--;
+                if(indegree[i] == 0) q.offer(i);
             }
         }
-
-        while (!q.isEmpty()) {
-            int studentNo = q.poll();
-
-            bw.write(String.valueOf(studentNo) + " ");
-
-            List<Integer> list = graph.get(studentNo);
-
-            for (int i = 0; i < list.size(); i++) {
-                int temp = list.get(i);
-                edgeCount[temp] -- ;
-                if (edgeCount[temp] == 0) {
-                    q.offer(temp);
-                }
-            }
-        }
-        bw.flush();
+        sb.setLength(sb.length()-1);
+        System.out.print(sb);
     }
 }
