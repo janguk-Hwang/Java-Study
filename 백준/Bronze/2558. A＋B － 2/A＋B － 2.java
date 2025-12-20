2c265dfa4f269b16347dc87dbcf92520c5a540fc
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2025-12-20 19:19:28 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        System.out.print(Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine()));
+    }
+}
