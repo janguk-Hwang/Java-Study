4ced1ae37229fc07164cb79c18df6cafe4f28736
janguk-Hwang <142196429+janguk-Hwang@users.noreply.github.com>
2026-04-05 19:39:53 +0900

    [Gold V] Title: 행복 유치원, Time: 552 ms, Memory: 52380 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -1,26 +1,23 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    static int n, k, total;
-    static int[] tall, diff;
-    static PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+    static int n, k;
+    static int[] arr, diff;
     static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws IOException{
-        st = new StringTokenizer(br.readLine(), " ");
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        tall = new int[n]; diff = new int[n];
+        arr = new int[n];
+        diff = new int[n - 1];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
-            tall[i] = Integer.parseInt(st.nextToken());
-            if(i > 0) {
-                diff[i] = tall[i] - tall[i-1];
-                pq.add(diff[i]);
-            }
-        }
-        total = tall[n-1] - tall[0];
-        for(int i=0; i<k-1; i++) total -= pq.poll();
-        System.out.println(total);
+        for(int i=0; i<n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<n-1; i++) diff[i] = arr[i+1] - arr[i];
+        Arrays.sort(diff);
+        int rst = 0;
+        for(int i=0; i<n-k; i++) rst += diff[i];
+        System.out.print(rst);
     }
 }
