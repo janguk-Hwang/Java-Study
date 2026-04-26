30b29aac9463e79a779620445d1e45b52bf093c3
janguk-Hwang <hwangw0210@gmail.com>
2026-04-26 20:14:08 +0900

    [Gold IV] Title: 팰린드롬?, Time: 692 ms, Memory: 218560 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,65 +1,38 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] num;
-    static int[][] dp;
-    static int N;
-    static int S;
-    static int E;
-
+    static int n, m;
+    static int[] arr;
+    static boolean[][] dp;
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        N = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split(" ");
-        num = new int[N + 1];
-        dp = new int[N + 1][N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            num[i] = Integer.parseInt(input[i - 1]);
-        }
-
-        palindrome();
-
-        int M = Integer.parseInt(br.readLine());
-        for (int i = 0; i < M; i++) {
-            String[] in = br.readLine().split(" ");
-            S = Integer.parseInt(in[0]);
-            E = Integer.parseInt(in[1]);
-
-            if (dp[S][E] == 1) {
-                bw.write("1\n");
-            } else {
-                bw.write("0\n");
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        dp = new boolean[n + 1][n + 1];
+        // 기저 사례(길이가 1인 구간은 무조건 팰린드롬)
+        for(int i=1; i<=n; i++) dp[i][i] = true;
+        // 기저 사례(길이가 2인 구간은 두 수가 같으면 팰린드롬)
+        for(int i=1; i<n; i++) if(arr[i] == arr[i + 1]) dp[i][i + 1] = true;
+        for(int i=3; i<=n; i++){    // 길이
+            for(int j=1; j<=n-i+1; j++){    // 인덱스
+                int k = j + i - 1;      // 구간의 끝 인덱스
+                // 내부 부분이 true이고 양 끝도 서로 같다면 팰린드롬
+                if(arr[j] == arr[k] && dp[j + 1][k - 1]) dp[j][k] = true;
             }
         }
-
-        bw.flush();
-        bw.close();
-    }
-
-    public static void palindrome() {
-        // 1. 길이가 1인 경우 모든 수는 자기 자신으로 팰린드롬이다.
-        for (int i = 1; i <= N; i++) {
-            dp[i][i] = 1;
+        m = Integer.parseInt(br.readLine());
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            if(dp[s][e]) sb.append("1").append("\n");
+            else sb.append("0").append("\n");
         }
-
-        // 2. 길이가 2인 경우 두 수가 같으면 팰린드롬이다.
-        for (int i = 1; i < N; i++) {
-            if (num[i] == num[i + 1]) {
-                dp[i][i + 1] = 1;
-            }
-        }
-
-        // 3. 길이가 3 이상인 경우 팰린드롬 판별
-        for (int len = 2; len < N; len++) {  // 길이를 2부터 늘려가며 검사
-            for (int start = 1; start + len <= N; start++) {
-                int end = start + len;
-                if (num[start] == num[end] && dp[start + 1][end - 1] == 1) {
-                    dp[start][end] = 1;
-                }
-            }
-        }
+        System.out.print(sb);
     }
 }
