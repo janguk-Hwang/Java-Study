47a4ef60787acdbf40e7b6bdeec83fd1aacc71c0
janguk-Hwang <hwangw0210@gmail.com>
2026-04-26 19:57:54 +0900

    [Gold V] Title: 빗물, Time: 108 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,46 +1,32 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+// 왼쪽 최대 높이와 오른쪽 최대 높이 중 더 낮은 쪽에 의해 결정
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static Stack<Integer> stack = new Stack<>();
+    static int h, w;
+    static int[] arr;
     static StringTokenizer st;
-    static int h, w, total;
-    static int[] height;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         h = Integer.parseInt(st.nextToken());
         w = Integer.parseInt(st.nextToken());
-        height = new int[w];
-        st = new StringTokenizer(br.readLine(), " ");
+        arr = new int[w];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<w; i++) arr[i] = Integer.parseInt(st.nextToken());
+        int rst = 0;
         for(int i=0; i<w; i++){
-            height[i] = Integer.parseInt(st.nextToken());
+            while(!stack.isEmpty() && arr[i] > arr[stack.peek()]){
+                int temp = stack.pop();     // 웅덩이의 가장 낮은 바닥 부분
+                if(stack.isEmpty()) break;  // 왼쪽 벽이 없으므로 물이 고일 수 없음
+                int left = stack.peek();    // 왼쪽 벽 인덱스
+                int width = i - left - 1;
+                int height = Math.min(arr[left], arr[i]) - arr[temp];
+                rst += width * height;
+            }
+            stack.push(i);
         }
-
-        total = 0;
-        solve();
-
-        System.out.println(total);
-    }
-
-    public static void solve(){
-        for(int i=1; i<w-1; i++){
-            int leftMax = 0;
-            int rightMax = 0;
-
-            // 왼쪽 최대값
-            for(int j=0; j<i; j++){
-                leftMax = Math.max(leftMax, height[j]);
-            }
-
-            // 오른쪽 최대값
-            for(int j=i+1; j<w; j++){
-                rightMax = Math.max(rightMax, height[j]);
-            }
-
-            int min = Math.min(leftMax, rightMax);
-            if(Math.min(leftMax, rightMax) > height[i]){
-                total += min - height[i];
-            }
-        }
+        System.out.print(rst);
     }
 }
