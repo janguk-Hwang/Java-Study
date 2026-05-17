de0261facf564e8a76f3df27a85f042c152b1ee8
janguk-Hwang <hwangw0210@gmail.com>
2026-05-17 20:08:01 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.60 ms, Memory: 63.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -0,0 +1,32 @@
+// k와 최단 거리의 홀짝성으로 사전에 가능성 확인
+// d, l, r, u가 사전순. 하좌우상
+import java.util.*;
+
+class Solution {
+    static int[] dx = {1, 0, 0, -1};
+    static int[] dy = {0, -1, 1, 0};
+    static char[] dir = {'d', 'l', 'r', 'u'};
+    static StringBuilder sb = new StringBuilder();
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        // 최단 거리
+        int dist = Math.abs(x - r) + Math.abs(y - c);
+        if(dist > k || (k - dist) % 2 != 0) return "impossible";
+        int tempX = x; int tempY = y;
+        for(int i=0; i<k; i++){
+            for(int d=0; d<4; d++){
+                int nx = tempX + dx[d];
+                int ny = tempY + dy[d];
+                if(nx >= 1 && nx <= n && ny >= 1&& ny <= m){
+                    int nDist = Math.abs(nx - r) + Math.abs(ny - c);
+                    if(nDist <= k - i - 1){
+                        sb.append(dir[d]);
+                        tempX = nx;
+                        tempY = ny;
+                        break;
+                    }
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
